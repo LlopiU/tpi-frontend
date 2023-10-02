2743d7a0e68a927e1aa4920109119e0ca04dab45
LlopiU <llopiulisesalan10@gmail.com>
2023-10-02 11:54:53 -0300

    Carreras
    Le di formas a la pagina de carreras

--- src/app/Carreras/page.js
+++ src/app/Carreras/page.js
@@ -2,34 +2,43 @@ function Carreras() {
     return (
       <>
         <div>
-              CARRERAS
-                CARRERAS DE GRADO
+              <h1 className="text-center font-bold">CARRERAS </h1>
+              <br/>
+               <h1 className="text-start font-bold"> CARRERAS DE GRADO </h1>
+               <br/>
 
-                Desde el pasado 01 de febrero hasta el 20 de febrero de 2023, inclusive, se encuentra abierta la inscripción al tercer turno del Seminario Universitario para ingresar a las carreras de grado que se dictan en la Facultad Regional Resistencia de UTN.
+                <p className="mx-auto pl-4">Desde el pasado 01 de febrero hasta el 20 de febrero de 2023, inclusive, se encuentra abierta la inscripción al tercer turno del Seminario Universitario para ingresar a las carreras de grado que se dictan en la Facultad Regional Resistencia de UTN.
 
-                Los interesados pueden acceder al formulario de preinscripción accediendo al link: www.frre.utn.edu.ar/academica/inscripcion
+                Los interesados pueden acceder al formulario de preinscripción accediendo al link: <a href="https://www.frre.utn.edu.ar/academica/inscripcion" class="hover:bg-gray-100 font-bold">www.frre.utn.edu.ar/academica/inscripcion</a>
 
-                Por consultas, dirigirse al mail: seminario@frre.utn.edu.ar
+                 <br/>Por consultas, dirigirse al mail: <a href="mailto:seminario@frre.utn.edu.ar" class="hover:bg-gray-100 font-bold">seminario@frre.utn.edu.ar</a> </p>
+              <br/>
+              <h1 className="text-start font-bold"> TECNICATURAS </h1>
+              <br/>
 
-                TECNICATURAS
+                <p className="mx-auto pl-4">También se encuentra abierta la inscripción a dos carreras cortas en la Facultad Regional Resistencia de UTN: Tecnicatura Universitaria en Mecatrónica y Tecnicatura Universitaria en Operación y Mantenimiento de Redes Eléctricas.
 
-                También se encuentra abierta la inscripción a dos carreras cortas en la Facultad Regional Resistencia de UTN: Tecnicatura Universitaria en Mecatrónica y Tecnicatura Universitaria en Operación y Mantenimiento de Redes Eléctricas.
+                La Tecnicatura en Mecatrónica, tiene una duración de dos años y medio y está diseñada para formar profesionales capacitados para entender y solucionar problemas relacionados a la producción, control y operación de equipos y máquinas, así como participar en actividades de mantenimiento y montaje en plantas industriales, de sistemas mecánicos, electrónicos, hidráulicos y combinaciones de los mismos. La preinscripción se realiza completando el formulario disponible en <a href="https://www.frre.utn.edu.ar/tsm" class="hover:bg-gray-100 font-bold">www.frre.utn.edu.ar/tsm</a>. Para mayor información, escribir a los correos <a href="mailto:tum@frre.utn.edu.ar" class="hover:bg-gray-100 font-bold">tum@frre.utn.edu.ar</a>
 
-                La Tecnicatura en Mecatrónica, tiene una duración de dos años y medio y está diseñada para formar profesionales capacitados para entender y solucionar problemas relacionados a la producción, control y operación de equipos y máquinas, así como participar en actividades de mantenimiento y montaje en plantas industriales, de sistemas mecánicos, electrónicos, hidráulicos y combinaciones de los mismos. La preinscripción se realiza completando el formulario disponible en www.frre.utn.edu.ar/tsm. Para mayor información, escribir a los correos tum@frre.utn.edu.ar
+                La Tecnicatura en Operación y Mantenimiento de Redes Eléctricas, por otra parte, surge de la necesidad que tiene la industria de transporte y distribución eléctrica de reforzar la formación de sus técnicos en determinados temas y en disminuir la brecha de formación entre el ingeniero y el técnico de nivel medio en el momento del ejercicio profesional. También tiene una duración de dos años y medio. La preinscripción se realiza completando el formulario disponible en <a href="https://www.frre.utn.edu.ar/tuomre" class="hover:bg-gray-100 font-bold">www.frre.utn.edu.ar/tuomre</a>. Para mayor información, escribir a los correos <a href="mailto:tuomre@frre.utn.edu.ar" class="hover:bg-gray-100 font-bold">tuomre@frre.utn.edu.ar</a> / <a href="mailto:tuomre.frre@gmail.com" class="hover:bg-gray-100 font-bold">tuomre.frre@gmail.com</a></p>
+            <br/>
+            <br/>
 
-                La Tecnicatura en Operación y Mantenimiento de Redes Eléctricas, por otra parte, surge de la necesidad que tiene la industria de transporte y distribución eléctrica de reforzar la formación de sus técnicos en determinados temas y en disminuir la brecha de formación entre el ingeniero y el técnico de nivel medio en el momento del ejercicio profesional. También tiene una duración de dos años y medio. La preinscripción se realiza completando el formulario disponible en www.frre.utn.edu.ar/tuomre. Para mayor información, escribir a los correos tuomre@frre.utn.edu.ar / tuomre.frre@gmail.com
-
-                CICLOS DE LICENCIATURA
-
+          
+              <h1 className="text-start font-bold">  CICLOS DE LICENCIATURA </h1>
+              <br/>
+              <p className="mx-auto pl-4">
                 La UTN cuenta además con ciclos de complementación curricular; actualmente se encuentra abierta la inscripción a las licenciaturas en Tecnología Educativa y en Producción de Bioimágenes hasta el 20 de febrero de 2023.
 
                 La Licenciatura en Tecnología Educativa, de dos años de duración y presentación de una tesina, está destinada a profesionales que deseen formarse en la planificación e implementación de proyectos de tecnología educativa en la enseñanza superior, producción de material educativo para situaciones propias de dicho nivel, conducción del proceso de enseñanza-aprendizaje y coordinación de equipos de trabajo en el ámbito de la investigación de la tecnología educativa.
 
-                Para preinscripción, completar el formulario disponible en www.frre.utn.edu.ar/academica/inscripcionclic. Para mayor información, escribir a los correos lte@frre.utn.edu.ar o lte_utn@yahoo.com.ar.
+                Para preinscripción, completar el formulario disponible en <a href="https://www.frre.utn.edu.ar/academica/inscripcionclic" class="hover:bg-gray-100 font-bold">www.frre.utn.edu.ar/academica/inscripcionclic</a>
+ . Para mayor información, escribir a los correos <a href="mailto:lte@frre.utn.edu.ar" class="hover:bg-gray-100 font-bold">lte@frre.utn.edu.ar</a> o <a href="mailto:lte_utn@yahoo.com.ar" class="hover:bg-gray-100 font-bold">lte_utn@yahoo.com.ar</a>
+.
 
                 La Licenciatura en Producción de Bioimágenes, por otra parte, está destinada a técnicos/as radiólogos/as o título similar otorgado por instituciones reconocidas, con una carga horaria mínima de 1.800 horas reloj y tres años de duración. Está diseñada para formar expertos en la aplicación clínica de las tecnologías involucradas en el diagnóstico por imágenes.
 
-                El formulario de preinscripción se encuentra online en el sitio www.frre.utn.edu.ar/academica/inscripcionclic. Para mayor información, escribir al correo lpb@frre.utn.edu.ar.
+                El formulario de preinscripción se encuentra online en el sitio <a href="https://www.frre.utn.edu.ar/academica/inscripcionclic" class="hover:bg-gray-100 font-bold">www.frre.utn.edu.ar/academica/inscripcionclic</a>. Para mayor información, escribir al correo <a href="mailto:lte_utn@yahoo.com.ar" class="hover:bg-gray-100 font-bold">lpb@frre.utn.edu.ar</a>.</p>
         </div>
       </>
     );
